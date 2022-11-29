b44369d21e3c4db648defb8b0c3694a7bdd79610
JeffLCoder <jeff_lei@outlook.com>
2022-11-29 16:10:20 -0500

    change text output from console.log to html

--- app.js
+++ app.js
@@ -14,30 +14,32 @@ const getComputerChoice = () => choices[Math.floor(Math.random() * choices.lengt
 
 //one round of the game
 function playRound(e) {
-    //check if player's input is valid, otherwise interrupt the process
+
     const playerSelection = e.target.name;
     const computerSelection = getComputerChoice();
-    if (playerSelection) {
-        // rock=0, paper=1, scissors=2, player wins only if the remainder is 1 or -2
-        switch (choices.indexOf(playerSelection) - choices.indexOf(computerSelection)) {
-            case 0:
-                console.log('Draw!');
-                scoreRecords.push({ 'computer': computerSelection, 'player': playerSelection, 'winner': 'none' });
-                break;
-            case 1:
-            case -2:
-                console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
-                scoreRecords.push({ 'computer': computerSelection, 'player': playerSelection, 'winner': 'player' });
-                playerScore += 1;
-                break;
-            default:
-                console.log(`You lose! ${playerSelection} beats ${computerSelection}!`);
-                scoreRecords.push({ 'computer': computerSelection, 'player': playerSelection, 'winner': 'computer' })
-                computerScore += 1;
-        }
+    announcement.appendChild(announce);
+    // rock=0, paper=1, scissors=2, player wins only if the remainder is 1 or -2
+    switch (choices.indexOf(playerSelection) - choices.indexOf(computerSelection)) {
+        case 0:
+            announce.textContent = 'Draw!';
+            scoreRecords.push({ 'computer': computerSelection, 'player': playerSelection, 'winner': 'none' });
+            break;
+        case 1:
+        case -2:
+            announce.textContent = `You win! ${playerSelection} beats ${computerSelection}!`;
+            scoreRecords.push({ 'computer': computerSelection, 'player': playerSelection, 'winner': 'player' });
+            playerScore += 1;
+            break;
+        default:
+            announce.textContent = `You lose! ${playerSelection} beats ${computerSelection}!`;
+            scoreRecords.push({ 'computer': computerSelection, 'player': playerSelection, 'winner': 'computer' })
+            computerScore += 1;
     }
-    return
+
 }
+
+const announce = document.createElement('p');
+const announcement = document.querySelector('.announcement');
 const buttons = document.querySelectorAll('.button');
 buttons.forEach(button => {
     button.addEventListener('click', playRound)
